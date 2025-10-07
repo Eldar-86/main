ceb4eb9d6a233517aaf30b0816bef3eb162d400d
Eldar-86 <223857862+Eldar-86@users.noreply.github.com>
2025-10-07 18:02:18 +0200

    Update word_guessing_game.py
    simple word guessing game

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -1,4 +1,6 @@
 import random
+
+
 def word_guess():
     word_list = ['kuća', 'knjiga', 'rijeka', 'grad', 'planina', 'more', 'automobil', 'škola', 'drvo', 'cvijet', 'zrakoplov',
      'telefon', 'ulica', 'mjesečina', 'sunce', 'dijamant', 'jednorog', 'tigar', 'igračka', 'kamera', 'balon', 'olovka',
@@ -9,21 +11,33 @@ def word_guess():
           'ruža', 'orhideja', 'ljubičica', 'maslina', 'kesten', 'orah', 'snijeg', 'ormar', 'led', 'vjetar', 'bura', 'duga',
            'oblaci', 'kiša', 'snješko', 'cijev', 'boja', 'crvena', 'plava', 'zelena', 'žuta', 'narandžasta', 'bijela', 'crna',
             'siva', 'smeđa', 'roza', 'ljubičasta', 'indigo', 'lubenica', 'jagoda', 'borovnica', 'grožđe', 'šljiva']
+
     word = random.choice(word_list)
-    answer = input(f"Ovo je riječ od {len(word)} slova, a prvo slovo je '{word[0]}'. Šta misliš koja je riječ? > ").lower()
-    if answer == word:
-        print("Tačno!")
-    else:
-        answer1 = input(f"Ne, ne baš. Drugo slovo je '{word[1]}'. Možeš li sada pogoditi?").lower()
-        if answer1 == word:
-            print("To je tačno!")
+    counter = 0
+    answer = input(f"Ovo je riječ od {len(word)} slova, a prvo slovo je '{word[counter]}'. Šta misliš koja je riječ? > ").lower()
+
+    while counter < 3:
+        if answer != word and counter < 2:
+            counter += 1
+            answer = input(f"Ne, ne baš. Iduće slovo je '{word[counter]}'. Možeš li sada pogoditi? > ")
+        elif answer != word and counter == 2:
+            print(f"Ne, riječ je {word}. Više sreće drugi put!")
+            restart = input("Želiš li ponovo pokušati? (d/n): ").lower()
+            if restart == 'd':
+                word_list.remove(word)
+                word_guess()
+            else:
+                print("Zatvaram program...")
+            break
         else:
-            print("Ne, nije ni ta. Možda ti pogađanje ne ide najbolje od ruke. Riječ u pitanju je", word)
-    restart = input("Želiš li ponovo pokušati? (d/n): ").lower()
-    if restart == 'd':
-        word_list.remove(word)
-        word_guess()
-    else:
-       print("Zatvaram program...")
+            print("Tačno!")
+            restart = input("Želiš li ponovo pokušati? (d/n): ").lower()
+            if restart == 'd':
+                word_list.remove(word)
+                word_guess()
+            else:
+                print("Zatvaram program...")
+            break
+
 
 word_guess()
